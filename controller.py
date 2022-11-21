ab9cb85727294f2cbbb173388045a6abfa5495fd
fernando <fernando@kakao.com>
2022-11-22 01:25:53 +0900

    Add functions for controller

--- controller.py
+++ controller.py
@@ -1,8 +1,10 @@
 import os
+import sys
 from dotenv import load_dotenv
 
 import auth
 import lotto645
+import notification
 
 def login(user_id: str, user_pw: str):
     globalAuthCtrl = auth.AuthController()
@@ -13,20 +15,57 @@ def login(user_id: str, user_pw: str):
 def buy_lotto645(authCtrl: auth.AuthController, cnt: int, mode: str):
     lotto = lotto645.Lotto645()
     _mode = lotto645.Lotto645Mode[mode.upper()]
-    lotto.buy_lotto645(authCtrl, cnt, _mode)
+    response = lotto.buy_lotto645(authCtrl, cnt, _mode)
+    return response
 
-def run():
+def check_winning_lotto645(authCtrl: auth.AuthController) -> dict:
+    lotto = lotto645.Lotto645()
+    item = lotto.check_winning(authCtrl)
+    return item
+
+def send_message(mode: lotto645.Lotto645Mode, response: dict, webhook_url: str):
+    notify = notification.Notification() 
+
+    if mode == lotto645.Lotto645Mode.CHECK:
+        notify.send_winning_message(response, webhook_url)
+    elif mode == lotto645.Lotto645Mode.BUY: 
+        notify.send_buying_message(response, webhook_url)
+
+def check(): 
+    load_dotenv() 
+
+    username = os.environ.get('USERNAME')
+    password = os.environ.get('PASSWORD')
+    slack_webhook_url = os.environ.get('SLACK_WEBHOOK_URL') 
+
+    authCtrl = login(username, password)
+    response = check_winning_lotto645(authCtrl)
+    send_message(lotto645.Lotto645Mode.CHECK, response, slack_webhook_url)
+
+def buy(): 
     
     load_dotenv() 
 
     username = os.environ.get('USERNAME')
     password = os.environ.get('PASSWORD')
     count = int(os.environ.get('COUNT'))
+    slack_webhook_url = os.environ.get('SLACK_WEBHOOK_URL') 
     mode = "AUTO"
 
     authCtrl = login(username, password)
-    buy_lotto645(authCtrl, count, mode)
+    response = buy_lotto645(authCtrl, count, mode)
+    send_message(lotto645.Lotto645Mode.BUY, response, slack_webhook_url)
 
+def run():
+    if len(sys.argv) < 2:
+        print("Usage: python controller.py [buy|check]")
+        return
+
+    if sys.argv[1] == "buy":
+        buy()
+    elif sys.argv[1] == "check":
+        check()
+  
 
 if __name__ == "__main__":
     run()
