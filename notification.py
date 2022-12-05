7ff016660a911620b69dd99cbff99b54aa2d2818
fernando <fernando@kakao.com>
2022-12-06 02:03:39 +0900

    Make lotto number formatting

--- notification.py
+++ notification.py
@@ -10,8 +10,23 @@ class Notification:
         if result.get("resultMsg", "FAILURE").upper() != "SUCCESS":  
             return
 
-        message = f"{result['buyRound']}회 로또 구매 완료 :moneybag:\n```{result['arrGameChoiceNum']}```"
+        lotto_number_str = self.make_lotto_number_message(result["arrGameChoiceNum"])
+        message = f"{result['buyRound']}회 로또 구매 완료 :moneybag:\n```{lotto_number_str}```"
         self._send_slack_webhook(webhook_url, message)
+
+    def make_lotto_number_message(self, lotto_number: list) -> str:
+        assert type(lotto_number) == list
+
+        # parse list without last number 3
+        lotto_number = [x[:-1] for x in lotto_number]
+        
+        # remove alphabet and | replace white space  from lotto_number
+        lotto_number = [x.replace("|", " ") for x in lotto_number]
+        
+        # lotto_number to string 
+        lotto_number = '\n'.join(x for x in lotto_number)
+        
+        return lotto_number
 
     def send_winning_message(self, winning: dict, webhook_url: str) -> None: 
         assert type(winning) == dict
